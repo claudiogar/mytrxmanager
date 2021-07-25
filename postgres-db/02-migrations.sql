d48f33535d804f9443223e258d293df37ac06396
Claudio Gargiulo <gargiulo.claudio@gmail.com>
2021-07-25 15:57:55 +0200

    Alter db column types for seamless EF conversions

--- postgres-db/02-migrations.sql
+++ postgres-db/02-migrations.sql
@@ -6,12 +6,12 @@
 BEGIN TRANSACTION;
 
 CREATE TABLE "Transactions" (
-    "Id" INTEGER NOT NULL CONSTRAINT "PK_Transactions" PRIMARY KEY,
-    "Date" TEXT NOT NULL,
-    "Amount" TEXT NOT NULL,
-    "Recipient" TEXT NULL,
-    "Currency" TEXT NULL,
-    "ProductType" INTEGER NOT NULL
+    "Id" SERIAL PRIMARY KEY,
+    "Date" timestamp without time zone NOT NULL,
+    "Amount" money NOT NULL,
+    "Recipient" text COLLATE pg_catalog."default",
+    "Currency" text COLLATE pg_catalog."default",
+    "ProductType" integer NOT NULL
 );
 
 INSERT INTO "__EFMigrationsHistory" ("MigrationId", "ProductVersion")
